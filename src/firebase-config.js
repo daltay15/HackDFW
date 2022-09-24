eda10cdc1bfd5aded9d58e03df80d538b40814b0
Geeknerd1337 <geeknerd1337@gmail.com>
2022-09-24 14:08:35 -0500

    Add React Toastify

---
+++ src/firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBY4OVEM_sDJc0P8ZiaZO-3yGmSJxpOcTw",
+  authDomain: "patient-portal-6e176.firebaseapp.com",
+  projectId: "patient-portal-6e176",
+  storageBucket: "patient-portal-6e176.appspot.com",
+  messagingSenderId: "1006902395795",
+  appId: "1:1006902395795:web:d844c061e17a2617285e66",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);

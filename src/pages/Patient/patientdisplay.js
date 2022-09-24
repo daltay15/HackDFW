610b27489d806cf15e82aeac2572b301725d741d
Geeknerd1337 <geeknerd1337@gmail.com>
2022-09-24 13:14:12 -0500

    Another RFix

--- src/pages/Patient/patientdisplay.js
+++ src/pages/Patient/patientdisplay.js
@@ -3,13 +3,22 @@ import React, { useState } from "react";
 import Navigation from "../../components/navigation";
 
 export default function PatientDisplay() {
+
+  const tabs = [
+    { title: "Home", href: "/patitnetdisplay" },
+    { title: "Food", href: "/menu" },
+    { title: "Translators", href: "/translate" },
+
+  ];
+
+
   return (
     <div className="h-screen w-screen bg-black">
     <div className="flex justify-center items-center bg-gray-200 h-full">
       Patient home page
       
     </div>
-    <Navigation/>
+    <Navigation tabs={tabs}/>
     </div>
   );
 }

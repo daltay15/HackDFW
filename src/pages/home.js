16b2cda28939718cd9827ff5fe8a0c39f127058a
alhassanmmohamed <104192448+alhassanmmohamed@users.noreply.github.com>
2022-09-24 16:15:59 -0500

    3/4 complete home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,15 +2,26 @@ import React, { useState } from "react";
 
 export default function Home() {
   return (
-    <div>
-      <img src="https://mma.prnewswire.com/media/1158375/Childrens_Health_Logo.jpg?p=facebook"></img>
-      <div className="flex items-center justify-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-20 border border-blue-700 rounded mt-[30px]">
-        <button>Patient</button>
+   <div>
+   <img
+              className="mx-auto h-auto w-auto"
+              src="https://mma.prnewswire.com/media/1158375/Childrens_Health_Logo.jpg?p=facebook"
+              alt="Children's Health Logo"
+            />
+       <div className = "flex items-center justify-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-20 border border-blue-700 rounded mt-[30px]">
+  <button>
+    Patient
+</button>
+</div>
 
-        <div className="flex items-center justify-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-20 border border-blue-700 rounded mt-[40px]">
-          <button>Staff</button>
-        </div>
-      </div>
-    </div>
-  );
+<div className  = "flex items-center justify-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-20 border border-blue-700 rounded mt-[40px]">
+  <button>
+    Staff
+
+    </button>
+
+
+       </div>
+       </div>
+         );
 }

d751b4e96453dda75e7ae4af559a5d217e57c3ff
Demir Altay <77849115+daltay15@users.noreply.github.com>
2022-09-24 08:09:52 -0500

    Updated Color for login page

--- src/pages/Patient/patientLogin.js
+++ src/pages/Patient/patientLogin.js
@@ -77,7 +77,7 @@ export default function patientLogin() {
             <div>
               <button
                 type="submit"
-                className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                className="group relative flex w-full justify-center rounded-md border border-transparent bg-red-600 py-2 px-4 text-sm font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
               >
                 Sign in
               </button>

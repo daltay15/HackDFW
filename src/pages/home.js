af999a71c016d865ccd5a49fb2681b18498e3c9b
Demir Altay <77849115+daltay15@users.noreply.github.com>
2022-09-24 17:54:10 -0500

    Bug fix

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
@@ -9,7 +10,9 @@ export default function Home() {
         alt="Children's Health Logo"
       />
       <div className="flex items-center justify-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-20 border border-blue-700 rounded mt-[30px]">
-        <button>Patient</button>
+        <Link to="/patientLogin">
+          <button>Patient</button>
+        </Link>
       </div>
 
       <div className="flex items-center justify-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-20 border border-blue-700 rounded mt-[40px]">

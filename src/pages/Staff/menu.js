e737889ef2d56c2d65a5bfe2ed62433ca074d1cc
Geeknerd1337 <geeknerd1337@gmail.com>
2022-09-24 13:00:25 -0500

    Menu fix

--- src/pages/Staff/menu.js
+++ src/pages/Staff/menu.js
@@ -1,7 +1,14 @@
 import React, { useState } from "react";
 import MenuView from "../../views/MenuView";
+import Navigation from "../../components/navigation";
 
 export default function Menu() {
+  const tabs = [
+    { title: "Home", href: "/staffdisplay" },
+    { title: "Food", href: "/menu" },
+    { title: "Translators", href: "/translate" },
+
+  ];
   return (
     <div className="h-screen w-screen bg-black">
     <MenuView/>

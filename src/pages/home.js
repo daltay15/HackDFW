76482939a0450640fb4c490fb17bf69ed6cbed87
alhassanmmohamed <104192448+alhassanmmohamed@users.noreply.github.com>
2022-09-24 18:54:03 -0500

    Bug Fix

--- src/pages/home.js
+++ src/pages/home.js
@@ -10,7 +10,9 @@ export default function Home() {
         alt="Children's Health Logo"
       />
       <div className="flex items-center justify-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-20 border border-blue-700 rounded mt-[30px]">
+        <Link to = "/patientlogin">
         <button>Patient</button>
+        </Link>
       </div>
 
       <div className="flex items-center justify-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-20 border border-blue-700 rounded mt-[40px]">

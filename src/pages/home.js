7210975e960c337a916fe699ec158cbeb5f642a5
alhassanmmohamed <104192448+alhassanmmohamed@users.noreply.github.com>
2022-09-24 18:13:20 -0500

    Final Home Page

--- src/pages/home.js
+++ src/pages/home.js
@@ -10,13 +10,13 @@ export default function Home() {
         alt="Children's Health Logo"
       />
       <div className="flex items-center justify-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-20 border border-blue-700 rounded mt-[30px]">
-        <Link to="/patientLogin">
-          <button>Patient</button>
-        </Link>
+        <button>Patient</button>
       </div>
 
       <div className="flex items-center justify-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-20 border border-blue-700 rounded mt-[40px]">
-        <button>Staff</button>
+        <Link to="stafflogin">
+          <button>Staff</button>
+        </Link>
       </div>
     </div>
   );

164d8ad1cc3fe2c0cf4dfa5f7112f0a8d93b8e37
Geeknerd1337 <geeknerd1337@gmail.com>
2022-09-24 18:00:59 -0500

    Update staffLogin.js

--- src/pages/Staff/staffLogin.js
+++ src/pages/Staff/staffLogin.js
@@ -1,9 +1,46 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { useDoctors } from "../../hooks/useDoctors";
+import { withRouter, useHistory } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-export default function staffLogin() {
+export default function StaffLogin() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { doctors } = useDoctors();
+  const history = useHistory();
+
+  function handleClick() {
+    history.push("/staffdisplay");
+  }
+
+  const Login = withRouter(({ history }) => (
+    <button
+      type="button"
+      className="group relative flex w-full justify-center rounded-md border border-transparent bg-red-600 py-2 px-4 text-sm font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+      onClick={() => {
+        history.push("/new-location");
+      }}
+    >
+      Sign In
+    </button>
+  ));
+
   return (
     <>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
       <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div className="w-full max-w-md space-y-8">
           <div>
@@ -23,6 +60,7 @@ export default function staffLogin() {
                 <input
                   className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
                   placeholder="Username"
+                  onChange={(e) => setUsername(e.target.value)}
                 />
               </div>
               <div>
@@ -34,6 +72,7 @@ export default function staffLogin() {
                   name="password"
                   type="password"
                   autoComplete="current-password"
+                  onChange={(e) => setPassword(e.target.value)}
                   required
                   className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
                   placeholder="Password"
@@ -68,14 +107,28 @@ export default function staffLogin() {
             </div>
 
             <div>
-              <Link to="/staffdisplay">
-                <button
-                  type="submit"
-                  className="group relative flex w-full justify-center rounded-md border border-transparent bg-red-600 py-2 px-4 text-sm font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                >
-                  Sign in
-                </button>
-              </Link>
+              <button
+                type="submit"
+                className="group relative flex w-full justify-center rounded-md border border-transparent bg-red-600 py-2 px-4 text-sm font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                onClick={(e) => {
+                  //THIS IS SO BAD
+                  //In a production environment we'd obviously use proper authentication
+                  //Filter to see if the username and password match otherwise show an error
+                  const doctor = doctors.filter(
+                    (doctor) =>
+                      doctor.username === username &&
+                      doctor.password === password
+                  );
+
+                  if (doctor.length === 0) {
+                    toast.error("Invalid username or password");
+                  } else {
+                    handleClick();
+                  }
+                }}
+              >
+                Sign in
+              </button>
             </div>
           </form>
         </div>

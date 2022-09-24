851979f18eb473b45ec9555f656015ea6729d54c
Demir Altay <77849115+daltay15@users.noreply.github.com>
2022-09-24 00:31:56 -0500

    First commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+import { Link } from "react-scroll";
+
+const Navbar = () => {
+  return <div className="font-bold">Hello world</div>;
+};
+
+export default Navbar;

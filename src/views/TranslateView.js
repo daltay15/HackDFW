1aaed39c46ecdd07dfc1536d4b238b9b06d22fbc
Demir Altay <77849115+daltay15@users.noreply.github.com>
2022-09-24 23:09:26 -0500

    Center Aligned Text

--- src/views/TranslateView.js
+++ src/views/TranslateView.js
@@ -1,6 +1,6 @@
 export default function TranslateView() {
   return (
-    <div className="flex justify-center items-center bg-gray-200 h-full">
+    <div className="flex justify-center items-center text-center bg-gray-200 h-full">
       <ul class="list-disc">
         <li>
           Now this is a story all about how, my life got flipped-turned upside
